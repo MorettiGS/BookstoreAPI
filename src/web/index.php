eff32376897fd8b98da832901b7a32a773aebb60
Gabriel <gabriel.souza@terceiro.rnp.br>
2024-09-17 22:14:32 -0300

    fix: gitignore

--- src/web/index.php
+++ src/web/index.php
@@ -3,7 +3,7 @@
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
 $dotenv->load();
 
 $config = require __DIR__ . '/../config/web.php';
